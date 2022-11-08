2e478189f6214a12810af062002dfdbb8d620e4e
MohMOG-ai <m.mograne@campus-igs-toulouse.fr>
2022-11-08 16:01:36 +0100

    random branch correction type

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
 // - choisir un mot random
 // Réinitialisation
-function newGame(type, data){
+function newGame(data){
     // On vide les différents champs de texte
     penduField.innerHTML = ''   
     msgField.innerHTML = ''     
@@ -20,14 +20,13 @@ function newGame(type, data){
     inputs = [];
     gameIsOver = false;
 
-    if (type == "array"){
-        // On prend un mot aléatoire dans le tableau des mots
-        let rand = Math.floor(Math.random() * data.words.length)
-        word = data.words[rand]
-        log(`Mot numéro ${rand}`)
-    } else { // Si on éxécute la fonction avec un argument, il récupérera cette valeur en tant que ce mot à trouver
-        word = data
-    }
+    
+    // On prend un mot aléatoire dans le tableau des mots
+    let rand = Math.floor(Math.random() * data.words.length)
+    word = data.words[rand]
+    log(`Mot numéro ${rand}`)
+    
+}
 
 // - mini prompt qui indique le nombre de lettre du mots + choix de la lettre
 
