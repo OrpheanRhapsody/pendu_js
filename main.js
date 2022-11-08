6eea20589bff50607ca7c3c5d4db71f65b4a6ca0
MohMOG-ai <m.mograne@campus-igs-toulouse.fr>
2022-11-08 15:14:51 +0100

    random branch draw

--- main.js
+++ main.js
@@ -4,7 +4,30 @@
 
 
 // - choisir un mot random
+// Réinitialisation
+function newGame(type, data){
+    // On vide les différents champs de texte
+    penduField.innerHTML = ''   
+    msgField.innerHTML = ''     
+    penduField.className=""     
 
+    // On réinitialise toutes les variables
+    wordArray = [];
+    pendu = [];
+
+    found = 0;
+    attempt = 0;
+    inputs = [];
+    gameIsOver = false;
+
+    if (type == "array"){
+        // On prend un mot aléatoire dans le tableau des mots
+        let rand = Math.floor(Math.random() * data.words.length)
+        word = data.words[rand]
+        log(`Mot numéro ${rand}`)
+    } else { // Si on éxécute la fonction avec un argument, il récupérera cette valeur en tant que ce mot à trouver
+        word = data
+    }
 
 // - mini prompt qui indique le nombre de lettre du mots + choix de la lettre
 
