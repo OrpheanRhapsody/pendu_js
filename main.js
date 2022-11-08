0d218f38c43de8b7165fcc5e603644634d480d28
rlevillain <robin.levillain@gmail.com>
2022-11-08 15:38:32 +0100

    version avec 1 ou 2

--- main.js
+++ main.js
@@ -8,12 +8,16 @@
 
 // - mini prompt qui indique le nombre de lettre du mots + choix de la lettre
 
+console.log(''.concat"Le mot à trouver fait :", mot_a_trouver.length()," caractères")
+let answer = console.log("Tapez 1 pour choisir une lettre ou tapez 2 pour trouver le mot ")
 
 
 // - rep(" _ ", length(motadeviner) )
 
 
 // - structure conditionnelles pour checker la lettre
+
+// essai de mot
 
 
 // - return nombre d'essais ... 
