a09e067f181fec56c2d73af392696cc26c0c5e2e
rlevillain <robin.levillain@gmail.com>
2022-11-08 16:02:14 +0100

    fonction mot caché

--- main.js
+++ main.js
@@ -11,7 +11,12 @@
 console.log(''.concat"Le mot à trouver fait :", mot_a_trouver.length()," caractères")
 let answer = console.log("Tapez 1 pour choisir une lettre ou tapez 2 pour trouver le mot ")
 
+mot_crypte = "_".repeat()
+function mot_cache(mot_a_trouver, mot_crypte,  mot_courant, lettre_choisie ){}  // mot à trouver est le mot generé et mot courant est le mot deviné enrichi des précédents choix de lettres
+     let position = mot_a_trouver.indexOf(lettre_choisie) 
+     if (position > 0) {
 
+     }
 // - rep(" _ ", length(motadeviner) )
 
 
