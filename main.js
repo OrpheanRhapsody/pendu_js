9c2775a8e3fe73818f8e7b3a17c2f1be6308e026
romain <romain.daveu@gmail.com>
2022-11-08 15:45:38 +0100

    isInWord function added

--- main.js
+++ main.js
@@ -14,6 +14,28 @@
 
 
 // - structure conditionnelles pour checker la lettre
+// let choiceLetter = "m"; // choix lettre user
+// let wordToGuess = "pomme"; // mot à diviner
+// let countTries = 15; 
+// let currentGuess = ""
+
+function isInWord(choiceLetter, wordToGuess){  
+  
+  if (wordToGuess.indexOf(choiceLetter.toLowerCase()) > -1)
+  {
+    console.log(`${choiceLetter} se trouve dans ${wordToGuess}.`);
+    console.log(`${wordToGuess.indexOf(choiceLetter.toLowerCase())}`)
+    console.log(`Il vous reste ${countTries} essais.`)
+
+  }
+  if (wordToGuess.indexOf(choiceLetter.toLowerCase()) <= -1)
+  {
+    console.log(`${choiceLetter} ne se trouve pas dans ${wordToGuess}`);
+
+  }
+}
+
+isInWord(choiceLetter, wordToGuess)
 
 
-// - return nombre d'essais ... 
+// - return nombre d'essais ... —
